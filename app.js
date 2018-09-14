a8d580ec06ca22dbf933618a7a9c6fc2d7084bf3
Lemmah <lemayiannakolah@gmail.com>
2018-09-14 11:06:49 +0300

    feat(webServer): create a http web server
    - use nodejs build in http.createServer to create a http server.
    - serve up some dummy data.
    
    [Delivers #webServer]

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 // Problem: We need an easy way to look at a Treehouse user's badge count and JavaScript points on the webrowser.
 // Solution: Use Nodejs to perform the profile look ups and serve our templates via HTTP
 
-// 1. Create a web server.
+// 1. Create a http web server.
+const http = require('http');
+
+/**
+ * Creates a http server that listens on port 8000.
+ */
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('This is before the end.\n');
+  res.end('Hello, World.\n');
+});
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(8000);
 
 
 // 2. Handle HTTP route GET / or POST / ie Home
