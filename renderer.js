61e9edb946b30e14c92c2c2c3d31738eb53b4e24
Lemmah <lemayiannakolah@gmail.com>
2018-09-16 14:34:21 +0300

    feat(templating): add functionality to template
    - create a function in the renderer to merge the specified values into the templates.
    - replate the template strings with the actual specified values.
    
    [Delivers #templating]

--- renderer.js
+++ renderer.js
@@ -1,12 +1,29 @@
-// 4. Function that handles the reading of files and merge in values
-  // read from file and get a string
-  // merge values into string
 const fs = require('fs');
 
+/**
+ * Replaces the template strings in the templates with the actual values.
+ * @param {Object} values - The user defined values to be rendered.
+ * @param {Object} content - The content of the target template file.
+ */
+function mergeValues(values, content) {
+  for (let value in values) {
+    content = content.replace(`{{${value}}}`, values[value]);
+  }
+
+  return content;
+}
+
+/**
+ * Handles the reading of files and merge in values.
+ * @param {String} templateName - The name of the template without the extension.
+ * @param {Object} values - The object that contains the values to inject into the template.
+ * @param {Object} response - The response Object of the request we're writting into.
+ */
 function view(templateName, values, response) {
-  // Read from the template file
-  const fileContents = fs.readFileSync(`./views/${templateName}.html`);
+  // Read from the template file : append the path and extension to the template name.
+  let fileContents = fs.readFileSync(`./views/${templateName}.html`, {encoding: 'utf8'});
   // Insert values into the content
+  fileContents = mergeValues(values, fileContents);
   // Write out content on Response
   response.write(fileContents);
 }
