4dac7891dff9373ff25d1bbe549cb9aaa008f277
Lemmah <lemayiannakolah@gmail.com>
2018-09-17 23:21:42 +0300

    feat(loadStyles): serve static css files
    - serve up static css files from disk.
    
    [Delivers #loadStyles]

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Problem: We need an easy way to look at a Treehouse user's badge count and JavaScript points on the webrowser.
 // Solution: Use Nodejs to perform the profile look ups and serve our templates via HTTP
-
+const fs = require('fs');
 const http = require('http');
 const routes = require('./scripts/router.js');
 
@@ -9,13 +9,20 @@ const routes = require('./scripts/router.js');
  */
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/html'});
+  // Check if url points to a user profile.
   const profileUrl = /^\/\w+$/.test(request.url);
+  // Check if url points to a static resource.
+  const staticUrl = /^\/\w+\/\w+\.\w+$/.test(request.url);
   if (request.url === '/') {
     routes.home(request, response);
   } else if (profileUrl) {
     routes.user(request, response);
+  } else if(staticUrl) {
+    const staticResource = fs.readFileSync(`.${request.url}`);
+    response.writeHead(200, {'Content-type': 'text/css'});
+    response.end(staticResource);
   } else {
-    response.end('Not found');
+    response.writeHead(303, {'Location': '/'});
   }
 });
 server.on('clientError', (err, socket) => {
